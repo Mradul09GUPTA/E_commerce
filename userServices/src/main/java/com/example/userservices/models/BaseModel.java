7a70286f770056582b4a04802b8c9e96fd778c17
Mradul Gupta <mradulgupta0911@gmail.com>
2026-04-28 19:31:32 +0530

    Fixed userServices submodule issue

---
+++ userServices/src/main/java/com/example/userservices/models/BaseModel.java
@@ -0,0 +1,20 @@
+package com.example.userservices.models;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date createdAt;
+    private Date lastUpdatedAt;
+    private State state;
+}
+
